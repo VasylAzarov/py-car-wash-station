d6ce49594bbf83590b00bc4a9a81710d37b48f86
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-08-31 16:13:56 +0200

    solution #6

--- app/CarWashStation.py
+++ app/CarWashStation.py
@@ -12,7 +12,7 @@ class CarWashStation:
     ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
-        self.average_rating = round(average_rating, 1)
+        self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: List[Car]) -> float:
